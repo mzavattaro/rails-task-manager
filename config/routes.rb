659155a812c9799f57014ec67a991671d8dcb68b
michael zavattaro <mwzavattaro@gmail.com>
2018-11-07 10:57:35 +1100

    Challenge 1 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index', as: :tasks # this creates tasks_path -> /tasks. the tasks_path is then used in the index.html.erb as one of the link_to arguments
+  get 'tasks/:id', to: 'tasks#show', as: :task # this creates task_path -> /task. the task_path is then used in the index.html.erb as one of the link_to arguments
+  post 'tasks', to: 'tasks#create', as: :new
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update', as: :update
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
+
+# task_path(task) -> /tasks/1
